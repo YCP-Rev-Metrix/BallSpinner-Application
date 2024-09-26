a83aa47cd9101a4d9b11aa841f6713a0cd0fc68a
Logan Tyran <logantyran6@gmail.com>
2024-09-26 11:51:14 -0400

    Set up basic solution with Front/Back & Test projects

---
+++ FrontEnd/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace FrontEnd;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using BackEnd;
+
+namespace Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        
+    }
+}
