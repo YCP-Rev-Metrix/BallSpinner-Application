7263a44074cd64bf824688b48a834f83f4fe199d
Logan Tyran <ltyran@ycp.edu>
2024-11-10 18:00:31 -0500

    TCP handshake

--- BackEnd/TCP.cs
+++ BackEnd/TCP.cs
@@ -17,11 +17,19 @@ namespace RevMetrix.BallSpinner.BackEnd;
 /// </summary>
 public class TCP : IDisposable
 {
+    /// <summary>
+    /// True if there is an existing TCP connection
+    /// </summary>
+    public bool Connected => _client.Connected;
+
     private TcpClient _client;
     private IPAddress _address;
 
     private const int BUFFER_SIZE = 1024;
     private const ushort PORT = 8411;
+
+    private readonly byte[] _receive = new byte[BUFFER_SIZE];
+    private readonly byte[] _send = new byte[BUFFER_SIZE];
 
     /// <summary/>
     public TCP(IPAddress address)
@@ -41,15 +49,43 @@ public class TCP : IDisposable
     {
         try
         {
-            await _client.ConnectAsync(_address, PORT);
-            await _client.Client.SendAsync(Encoding.UTF8.GetBytes("Hello from the application!"));
+            //await _client.ConnectAsync(_address, PORT);
+            byte token = (byte)new Random().Next(255);
+
+            //Handshake by telling BallSpinner that I want to connect
+            _send[0] = (byte)MessageType.Connect;
+            _send[1] = 1; //No data to send
+            _send[2] = token; //Random token, BallSpinner MUST send back the same value
+
+            int sent = await _client.Client.SendAsync(new ArraySegment<byte>(_send, 0, 3));
+            if (sent <= 0)
+                throw new Exception("Handshake request could not be sent");
+
+            //Receive ball spinner data, if data is not receieved the connection was rejected
+            int receieved = await _client.Client.ReceiveAsync(_receive);
+            if (receieved <= 0)
+                throw new Exception("Handshake request could not be received");
+
+            if (_receive[0] != (byte)MessageType.ConnectResponse)
+                throw new Exception("Host sent wrong data! Handshake failed.");
+
+            if (_receive[1] != 1)
+                throw new Exception("Host sent too much data! Handshake failed.");
+
+            if (_receive[2] != token)
+                throw new Exception("Host did not respond with same token! Handshake failed.");
+
+            //02 00 FF
+            byte messageType = _receive[0];
+            int messageSize = _receive[1] + (_receive[2] << sizeof(byte));
+
+            if (_client.Connected)
+                Listen();
         }
         catch (Exception e)
         {
             Console.WriteLine(e.ToString());
         }
-
-        Listen();
     }
 
     private async void Listen()
@@ -74,4 +110,25 @@ public class TCP : IDisposable
                 Debug.WriteLine(Encoding.UTF8.GetString(buffer, 0, size));
         }
     }
+}
+
+/// <summary>
+/// Types of messages that can be sent to the ball spinner
+/// </summary>
+public enum MessageType : byte
+{
+    /// <summary>
+    /// Connect to the ball spinner
+    /// </summary>
+    Connect = 0x01,
+
+    /// <summary>
+    /// Connect to the ball spinner
+    /// </summary>
+    ConnectResponse = 0x02,
+
+    /// <summary>
+    /// Send raw text
+    /// </summary>
+    Text =  0xFF,
 }
