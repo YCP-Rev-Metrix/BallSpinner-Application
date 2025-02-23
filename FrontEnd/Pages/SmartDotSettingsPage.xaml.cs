b21447b1313606f7d33489e9444c51653800179c
notPatrick9 <pdevine2@ycp.edu>
2025-02-23 17:32:55 -0500

    Fixed binding context for SmartDot Settings page and autopopulating pickers

--- FrontEnd/Pages/SmartDotSettingsPage.xaml.cs
+++ FrontEnd/Pages/SmartDotSettingsPage.xaml.cs
@@ -17,6 +17,19 @@ public partial class SmartDotSettingsPage : ContentPage
 
         BindingContext = _viewModel;
         InitializeComponent();
+
+        //Autopopulate the pickers to index 0 of their binding context
+        AccelrationRange.SelectedIndex = 0;
+        AccelrationSampleRate.SelectedIndex = 0;
+
+        RotationRange.SelectedIndex = 0;
+        RotationSampleRate.SelectedIndex = 0;
+
+        MagnatometerRange.SelectedIndex = 0;
+        MagnatometerSampleRate.SelectedIndex = 0;
+
+        LightRange.SelectedIndex = 0;
+        LightSampleRate.SelectedIndex = 0;
     }
 
     private void OnSubmitButtonClicked(object sender, EventArgs e)
--- FrontEnd/SmartDotSettingsViewModel.cs
+++ FrontEnd/SmartDotSettingsViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using RevMetrix.BallSpinner.BackEnd.BallSpinner;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,13 +11,63 @@ namespace RevMetrix.BallSpinner.FrontEnd;
 internal class SmartDotSettingsViewModel
 {
     private IBallSpinner _ballSpinner = null;
-    public List<List<double>> Ranges;
-    public List<List<double>> SampleRates;
+    public ObservableCollection<List<double>> Ranges { get; private set; }
+    public ObservableCollection<List<double>> SampleRates { get; private set; }
 
     public SmartDotSettingsViewModel(IBallSpinner ballSpinner)
     {
         _ballSpinner = ballSpinner;
-        Ranges = _ballSpinner.GetAvailableRanges();
-        SampleRates = _ballSpinner.GetAvailableSampleRates();
+
+        Ranges = new ObservableCollection<List<double>>();
+        SampleRates = new ObservableCollection<List<double>>();
+
+        BuildRanges();
+        BuildSampleRates();
+    }
+
+    public void BuildRanges()
+    {
+        Ranges.Clear();
+
+        var RangesList = _ballSpinner.GetAvailableRanges();
+
+        if (RangesList != null)
+        {
+            for (int i = 0; i < RangesList.Count; i++)
+            {
+                Ranges.Add(new List<double>());
+
+                if (RangesList[i] != null)
+                {
+                    foreach (double val in RangesList[i])
+                    {
+                        Ranges[i].Add(val);
+                    }
+                }
+            }
+        }
+    }
+
+    public void BuildSampleRates()
+    {
+        SampleRates.Clear();
+
+        var SampleRateList = _ballSpinner.GetAvailableSampleRates();
+
+        if (SampleRateList != null)
+        {
+            for (int i = 0; i < SampleRateList.Count; i++)
+            {
+                SampleRates.Add(new List<double>());
+
+                if (SampleRateList[i] != null)
+                {
+                    foreach (double val in SampleRateList[i])
+                    {
+                        SampleRates[i].Add(val);
+                    }
+                }
+            }
+        }
     }
 }
