02d9b16ba47d7b6de25dd933aa965a9cf2519d4b
notPatrick9 <pdevine2@ycp.edu>
2025-04-15 17:24:37 -0400

    Updated registration field checking

--- FrontEnd/Pages/LoginPage.xaml.cs
+++ FrontEnd/Pages/LoginPage.xaml.cs
@@ -19,13 +19,14 @@ public partial class LoginPage : ContentPage
 
     private async void OnRegisterButtonClicked(object sender, EventArgs args)
     {
-        var firstName = RegisterFirstNameField.Text;
-        var lastName = RegisterLastNameField.Text;
-        var email = RegisterEmailField.Text;
-        var phoneNum = RegisterPhoneNumField.Text;
-        var username = RegisterUsernameField.Text;
-        var password = RegisterPasswordField.Text;
-        var confirmPassword = RegisterConfirmPasswordField.Text;
+        string firstName = RegisterFirstNameField.Text;
+        string lastName = RegisterLastNameField.Text;
+        string email = RegisterEmailField.Text;
+        string phoneNum = RegisterPhoneNumField.Text;
+        string username = RegisterUsernameField.Text;
+        string password = RegisterPasswordField.Text;
+        string confirmPassword = RegisterConfirmPasswordField.Text;
+        int dummyInt;
 
         if (string.IsNullOrEmpty(firstName) || 
             string.IsNullOrEmpty(lastName)  || 
@@ -37,14 +38,37 @@ public partial class LoginPage : ContentPage
         {
             await DisplayAlert("Alert", "You have not entered compleated all fields", "Fine");
         }
-        else if (RegisterPasswordField.Text.CompareTo(RegisterConfirmPasswordField.Text) != 0)
+        else if (password.CompareTo(confirmPassword) != 0)
         {
             await DisplayAlert("Alert", "Passwords do not match", "UGHHHHHHHHH");
         }
+        else if (!email.Contains('@'))
+        {
+            await DisplayAlert("Alert", "Improper email", "UGHHHHHHHHH");
+        }
+        else if (phoneNum.Length != 10 || !phoneNum.All(char.IsDigit))
+        {
+            await DisplayAlert("Alert", "Improper phone number", "UGHHHHHHHHH");
+        }
+        else
         {
             try
             {
-                var token = await _database.RegisterUser(firstName, lastName, username, password, email, phoneNum);
+                char[] phoneNumArr = phoneNum.ToCharArray();
+                string formattedPhoneNum = phoneNumArr[0] + 
+                                           phoneNumArr[1] + 
+                                           phoneNumArr[2] + 
+                                           "-" +
+                                           phoneNumArr[3] +
+                                           phoneNumArr[4] +
+                                           phoneNumArr[5] +
+                                           "-" +
+                                           phoneNumArr[6] +
+                                           phoneNumArr[7] +
+                                           phoneNumArr[8] +
+                                           phoneNumArr[9];
+
+                var token = await _database.RegisterUser(firstName, lastName, username, password, email, formattedPhoneNum);
                 Console.WriteLine($"Auth: {token?.TokenA}");
                 Console.WriteLine($"Refresh: {token?.TokenB}");
             }
